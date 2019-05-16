2199df6af8547c206c737b7a0f47d2953f4b4df3
Baptiste Rosillon <batros.web@gmail.com>
2019-05-16 15:55:03 +0200

    add task to bot and refactor command processing

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,15 +1,17 @@
 import discord
 import config as cfg
+import asyncio
 
 from discord.ext import commands, tasks
 
+# @TODO need to work only with private messages
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
 
 bot = commands.Bot(command_prefix='!', description=description)
-userList=[]
+taskList = {}
 
 @bot.event
 async def on_ready():
@@ -18,32 +20,50 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
-        print(message.author)
-        await bot.process_commands(message)
+        await exec_commands(message)
+        #await bot.process_commands(message)
 
-@bot.command()
-async def start(ctx):
-    print(ctx.author)
-    if ctx.author in userList:
-        await ctx.send("L'envois de formulaire est deja activé")
-    else:
-        userList.append(ctx.author)
-        await ctx.send("Je vais t'envoyer le formulaire au cours de la journée")
+async def exec_commands(message):
+    # Do not execute function if it's not a DM
+    if not isinstance(message.channel, discord.channel.DMChannel):
+        return
 
-@bot.command()
-async def stop(ctx):
-    if ctx.author in userList:
-        userList.remove(ctx.author)
-        await ctx.send("L'envois de formulaire est désactivé")
-    else:
-        await ctx.send("L'envois de formulaire n'était pas activé")
+    # Start the task : send link to online form
+    if message.content == 'start':
+        if str(message.channel.recipient.id) in taskList:
+            await message.channel.send("L'envois de formulaire est deja activé")
+        else:
+            taskList[str(message.channel.recipient.id)] = asyncio.ensure_future(send_form(message.channel))
+            await message.channel.send("Je vais t'envoyer le formulaire au cours de la journée")
 
-@bot.command()
-async def status(ctx):
-    if ctx.author in userList:
-        await ctx.send("L'envois de formulaire est activé")
-    else:
-        await ctx.send("L'envois de formulaire est désactivé")
+    # Stop task
+    elif message.content == 'stop':
+        if str(message.channel.recipient.id) in taskList:
+            taskList[str(message.channel.recipient.id)].cancel()
+            del taskList[str(message.channel.recipient.id)]
+            await message.channel.send("L'envois de formulaire est désactivé")
+        else:
+            await message.channel.send("L'envois de formulaire n'était pas activé")
 
+    # Send status to user
+    elif message.content == 'status':
+        if str(message.channel.recipient.id) in taskList:
+            await message.channel.send("L'envois de formulaire est activé")
+        else:
+            await message.channel.send("L'envois de formulaire est désactivé")
+
+async def send_form(channel):
+    await asyncio.sleep(5)
+    await channel.send("Voila le formulaire : {}".format(cfg.FORM_URL))
+    await send_form(channel)
+
+def dump(obj):
+   """
+    Use for debug : dump an objects properties and methods
+   :param obj:
+   """
+   for attr in dir(obj):
+       if hasattr( obj, attr ):
+           print( "obj.%s = %s" % (attr, getattr(obj, attr)))
 
 bot.run(cfg.TOKEN)
