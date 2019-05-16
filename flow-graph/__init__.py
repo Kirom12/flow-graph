aa1906c097213f0984a43bff5e48990b46c9ec90
Baptiste Rosillon <batros.web@gmail.com>
2019-05-16 19:44:55 +0200

    correct bug on deployment

--- flow-graph/__init__.py
+++ flow-graph/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(environ=None, start_response=None, test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
