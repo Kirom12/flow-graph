ef8b7492a34c54f43cf95b8533a75873178cffe5
Baptiste Rosillon <batros.web@gmail.com>
2019-05-19 23:08:45 +0200

    change configuration

--- flow-graph/default_settings.py
+++ flow-graph/default_settings.py
@@ -1,3 +1,3 @@
 SECRET_KEY='dev'
-PERSONAL_TOKEN = "tm7njCywGR4qkRDpcAye48DWTbxGGawZ5qLfQwhF9KX"
-TYPEFORM_API_URL = "https://api.typeform.com/forms/YNXWSn/responses"
+PERSONAL_TOKEN = ""
+TYPEFORM_API_URL = ""
